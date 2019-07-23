7420ecb52cf9320f8e41ad3f600c4701d630aeff
Khel Kim <cafehero123@gmail.com>
2019-07-23 13:59:50 +0900

    solve 6026 Dynamic Programming

---
+++ SWExpertAcademy/6026.py
@@ -0,0 +1,43 @@
+T = int(input())
+data = []
+for _ in range(T):
+    data.append(list(map(int, input().split())))
+
+
+def combination(n, k):
+    w = [[1]]
+    k = min(k, n-k)
+    for i in range(1, n + 1):
+        w.append([])
+        for j in range(min(i, k) + 1):
+            if j == 0:
+                w[-1].append(1)
+            elif j == i:
+                w[-1].append(1)
+            else:
+                w[-1].append(w[-2][j-1] + w[-2][j])
+    return w[n][k]
+
+
+def dp(m, n):
+    memo = [0] * (m + 1)
+    for i in range(1, m+1):
+        memo[i] += i ** n
+        for j in range(1, i):
+            memo[i] -= combination2(i, j) * memo[j]
+    return memo[-1]
+
+
+def combination2(n, m):
+    upper = 1
+    for i in range(m+1, n +1):
+        upper *= i
+    down = 1
+    for j in range(1, n - m + 1):
+        down *= j
+    return upper // down
+
+
+for i, case in enumerate(data):
+    m, n = case
+    print("#%s" % (i + 1), dp(m,n) % 1000000007)
