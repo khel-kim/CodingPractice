a8d5a84d363210bc5157a7d8739176f8f0d8c822
Khel Kim <cafehero123@gmail.com>
2019-07-30 09:15:55 +0900

    solve 13458(2)

--- BackJoon/simulation/13458.py
+++ BackJoon/simulation/13458.py
@@ -3,14 +3,4 @@ n = int(input())
 a = list(map(int, input().split()))
 b, c = map(int, input().split())
 
-count = n
-tmp1 = [i - b if i - b > 0 else 0 for i in a]
-tmp2 = [math.ceil(j / c) for j in tmp1]
-
-print(count + sum(tmp2))
-
-"""
-3
-1 1 1
-2 2
-"""
+print(n + sum([math.ceil((i - b) / c) if i - b > 0 else 0 for i in a]))
