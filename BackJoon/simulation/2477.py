3d5597f59e1b53de3b3f8f9fb3445b465f039fd5
khelkim <cafehero123@gmail.com>
2019-08-27 15:07:41 +0900

    20190827 solve 2477

--- BackJoon/simulation/2477.py
+++ BackJoon/simulation/2477.py
@@ -4,41 +4,23 @@ even_lines = []
 odd_lines = []
 for i in range(6):
     tmp = list(map(int, input().split()))
-    if i & 1 == 0:
-        even_lines.append(tmp[1])
-    else:
-        odd_lines.append(tmp[1])
+    if i & 1 == 0: even_lines.append(tmp[1])
+    else: odd_lines.append(tmp[1])
     lines.append(tmp)
 normal = [4, 2, 3, 1]
 height = max(even_lines)
 width = max(odd_lines)
 
-start = lines[0][0]
-start_index = normal.index(start)
+start_index = normal.index(lines[0][0])
 corner = start_index
 for i in range(1, 6):
-    corner += 1
-    corner = corner % 4
+    corner = (corner + 1) % 4
     if normal[corner] != lines[i][0]:
         corner = i
         break
 else:
-    corner = start_index
+    corner = 0
 before_corner = (corner - 1) % 6
-
-
 except_area = lines[before_corner][1] * lines[corner][1]
-print(corner)
-print(height, width, lines[before_corner][1], lines[corner][1])
+
 print((height * width - except_area) * k)
-
-
-"""
-7
-1 100
-4 10
-2 160
-3 50
-1 60
-4 40
-"""
