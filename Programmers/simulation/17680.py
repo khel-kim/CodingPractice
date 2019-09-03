1f5cc005806413f684e28227f27b8fa06f8b0113
khel kim <cafehero123@gmail.com>
2019-09-03 16:17:29 +0900

    solve 17680

--- Programmers/simulation/17680.py
+++ Programmers/simulation/17680.py
@@ -1,32 +1,23 @@
-cacheSize = 3
-cities = ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA']
-
-from collections import deque
-cache = deque([])
-time = 0
-for i in range(len(cities)):
-    city = cities[i].lower()
-
-    flag = False
-    for index in range(len(cache)):
-        if cache[index] == city:
-            flag = True
-            cache.append(cache[index])
-            cache.pop(index)
-            break
-
-    if flag:
-        time += 1
-    else:
-        time += 5
-
-    if flag:
-        continue
-    else:
+def solution(cacheSize, cities):
+    cache = []
+    time = 0
+    for i in range(len(cities)):
+        city = cities[i].lower()
+        flag = False
+        for index in range(len(cache)):
+            if cache[index] == city:
+                flag = True
+                tmp = cache.pop(index)
+                cache.append(tmp)
+                time += 1
+                break
+        else:
+            time += 5
+        if flag: continue
         if len(cache) < cacheSize:
             cache.append(city)
         else:
             if not cache: continue
-            cache.popleft()
+            cache.pop(0)
             cache.append(city)
-print(time)
+    return time
