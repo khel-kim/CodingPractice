c48d20cdbd10e5e5c2d55bfdbbf72b37fe224608
Khel Kim <cafehero123@gmail.com>
2019-09-18 17:53:49 +0900

    add 2643

--- test.py
+++ test.py
@@ -1,10 +1,41 @@
-from itertools import combinations, permutations
-
-arr = [1, 2, 3, 4, 5]
-count = 1
-for front_end in combinations(arr, 2):
-    perm_candi = [i for i in arr if i not in front_end]
-    for middle in permutations(perm_candi, len(perm_candi)):
-        new_arr = [front_end[0]] + list(middle) + [front_end[1]]
-        print(count, new_arr)
+def permute(arr):
+    result = [arr[:]]
+    c = [0] * len(arr)
+    count = 0
+    i = 0  # start
+    while i < len(arr):
         count += 1
+        print("countcount", count, "*" * 100)
+        print(i)
+        print(c)
+        print(arr,"before")
+        print(result, "before")
+        if c[i] < i:
+            if i % 2 == 0:
+                arr[0], arr[i] = arr[i], arr[0]
+            else:
+                arr[c[i]], arr[i] = arr[i], arr[c[i]]
+            result.append(arr[:])
+            c[i] += 1
+            i = 0
+        else:
+            c[i] = 0
+
+            i += 1
+        print("%" * 20)
+        print(arr, "after")
+        print(result, "after")
+    return result
+
+
+arr = [i for i in range(1, 4)]
+print(len(permute(arr)))
+
+import itertools
+
+permutations = itertools.permutations
+
+for i, arr in enumerate(permutations(arr)):
+    # print(i+1, arr)
+    pass
+print(i+1)
