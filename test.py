d1925ae4298c1b4d23dcf58f12c718ab1bdc9f32
Khel Kim <cafehero123@gmail.com>
2019-09-20 11:36:28 +0900

    solve programmers 43238

--- test.py
+++ test.py
@@ -1,41 +1,31 @@
-def permute(arr):
-    result = [arr[:]]
-    c = [0] * len(arr)
-    count = 0
-    i = 0  # start
-    while i < len(arr):
-        count += 1
-        print("countcount", count, "*" * 100)
-        print(i)
-        print(c)
-        print(arr,"before")
-        print(result, "before")
-        if c[i] < i:
-            if i % 2 == 0:
-                arr[0], arr[i] = arr[i], arr[0]
+def solution(n, times):
+    minimum = min(times)
+    start = minimum
+    end = min(minimum * n + 1, (n // len(times)+1) * max(times))
+    while start <= end:
+        index = (start + end) // 2
+        count = 0
+        before_count = 0
+        # print(start, end, index)
+        for time in times:
+            count += index // time
+            before_count += (index - 1) // time
+            if count > n:
+                break
+        if count == n:
+            if before_count == n - 1:
+                return index
             else:
-                arr[c[i]], arr[i] = arr[i], arr[c[i]]
-            result.append(arr[:])
-            c[i] += 1
-            i = 0
+                end = index
+        elif count > n:
+            end = index
         else:
-            c[i] = 0
-
-            i += 1
-        print("%" * 20)
-        print(arr, "after")
-        print(result, "after")
-    return result
+            start = index
+        if start == end:
+            return start
+        elif end - start == 1:
+            return end
 
 
-arr = [i for i in range(1, 4)]
-print(len(permute(arr)))
 
-import itertools
-
-permutations = itertools.permutations
-
-for i, arr in enumerate(permutations(arr)):
-    # print(i+1, arr)
-    pass
-print(i+1)
+print(solution(1, [7, 10]))
